fb2f83d048345fa9999ee4f7a985509828b63631
Alexander von Below <alex@vonbelow.com>
2022-10-20 16:48:02 +0200

    Widget Added

---
+++ SampleWidget/SampleWidget.swift
@@ -0,0 +1,66 @@
+//
+//  SampleWidget.swift
+//  SampleWidget
+//
+//  Created by Alexander von Below on 20.10.22.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct SampleWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+struct SampleWidget: Widget {
+    let kind: String = "SampleWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            SampleWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct SampleWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        SampleWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
---
+++ SampleWidget/SampleWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SampleWidgetBundle.swift
+//  SampleWidget
+//
+//  Created by Alexander von Below on 20.10.22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SampleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SampleWidget()
+    }
+}
