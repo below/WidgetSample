5890e5ace8215c3a8491e1e4c880a9813020b1ae
Alexander von Below <alex@vonbelow.com>
2022-10-20 16:28:48 +0200

    Initial Commit

---
+++ WidgetSample/WidgetSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetSampleApp.swift
+//  WidgetSample
+//
+//  Created by Alexander von Below on 20.10.22.
+//
+
+import SwiftUI
+
+@main
+struct WidgetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
